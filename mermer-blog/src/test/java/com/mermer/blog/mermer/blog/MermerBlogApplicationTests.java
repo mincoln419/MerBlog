7e0b16d2fc121a4eda1f2b45988553ce7723d886
mermer <mincoln419@naver.com>
2022-02-06 21:04:09 +0900

    #2 blog rest-api init
    #2 blog rest-api init

---
+++ mermer-blog/src/test/java/com/mermer/blog/mermer/blog/MermerBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mermer.blog.mermer.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MermerBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
