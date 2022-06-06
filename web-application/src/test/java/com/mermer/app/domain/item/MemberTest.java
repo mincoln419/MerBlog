fe4a86a4bfe1115c51384de31915225e5716c820
mermer vivo <mincoln419@naver.com>
2022-06-07 07:11:32 +0900

    refs #40 기본 도메인 설계
    refs #40 기본 도메인 설계

--- web-application/src/test/java/com/mermer/app/domain/item/MemberTest.java
+++ web-application/src/test/java/com/mermer/app/domain/item/MemberTest.java
@@ -1,5 +1,7 @@
 package com.mermer.app.domain.item;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -7,11 +9,13 @@ import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 
 import com.mermer.app.domain.Member;
 import com.mermer.app.domain.Team;
+import com.mermer.app.repository.MemberJpaRepository;
 
 @SpringBootTest
 @Transactional
@@ -20,6 +24,10 @@ class MemberTest {
 
 	@PersistenceContext
 	EntityManager em;
+	
+	@Autowired
+	MemberJpaRepository memberJpaRepository;
+	
 	
 	@Test
 	void testEntity() {
@@ -51,6 +59,30 @@ class MemberTest {
 			
 		});
 		
+	}
+	
+	@Test
+	public void basicCRUD() {
+		
+		Team teamA = new Team("teamA");
+		Team teamB = new Team("teamB");
+		em.persist(teamA);
+		em.persist(teamB);
+		
+		Member member1 = new Member("member1", 10, teamA);
+		Member member2 = new Member("member2", 12, teamB);
+		
+		memberJpaRepository.save(member1);
+		memberJpaRepository.save(member2);
+		
+		Member findMember1 = memberJpaRepository.findById(member1.getId()).get();
+		Member findMember2 = memberJpaRepository.findById(member2.getId()).get();
+		
+		assertThat(findMember1).isEqualTo(member1);
+		assertThat(findMember2).isEqualTo(member2);
+		
+		assertThat(memberJpaRepository.count()).isEqualTo(memberJpaRepository.findAll().size());
+
 	}
 
 }
