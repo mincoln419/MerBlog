98b0609848f2f81b3ecccb56ce03cf0f7a3bae05
mermer vivo <mincoln419@naver.com>
2022-06-12 18:20:24 +0900

    refs #38 web 확장 page와 정렬
    refs #38 web 확장 page와 정렬

--- web-application/src/main/java/com/mermer/app/api/MemberJpaController.java
+++ web-application/src/main/java/com/mermer/app/api/MemberJpaController.java
@@ -1,10 +1,15 @@
 package com.mermer.app.api;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mermer.app.domain.Member;
+import com.mermer.app.domain.MemberDto;
 import com.mermer.app.repository.MemberRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -27,5 +32,10 @@ public class MemberJpaController {
 	public String findMember2(@PathVariable("id") Member member) {
 		return member.getName();
 		
+	}
+	//@Qualifier 쓰면 페이징 여러개로 사용할 수 있음
+	@GetMapping("/membersAll")
+	public Page<MemberDto> list(@Qualifier("mermer") @PageableDefault(page = 0, size = 1000) Pageable pageable){
+		return memberRepository.findAll(pageable).map(member -> new MemberDto(member));
 	}
 }
